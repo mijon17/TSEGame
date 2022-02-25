6cf5b113e38cf8ad4958fd2653ce21e39fcbb58c
ShootyBoi <79335581+ShootyBoi@users.noreply.github.com>
2022-02-25 18:09:15 +0000

    redo

--- PlatformerSouls.V4/Assets/Scripts/UIScripts/UIManager.cs
+++ PlatformerSouls.V4/Assets/Scripts/UIScripts/UIManager.cs
@@ -1,11 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class UIManager : MonoBehaviour
 {
-    
+    public List<GameObject> buttonElements = new List<GameObject>();
     public GameObject background;
 
     void Start()
