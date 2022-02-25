db866beeeaeea2c338fb3bc64a2d7c8d8b313400
ShootyBoi <79335581+ShootyBoi@users.noreply.github.com>
2022-02-25 19:09:37 +0000

    index implementation

--- PlatformerSouls.V4/Assets/Scripts/UIScripts/UIManager.cs
+++ PlatformerSouls.V4/Assets/Scripts/UIScripts/UIManager.cs
@@ -2,10 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
-public class UIManager : MonoBehaviour
+
+//this is predominanently for using keys to interact
+public class UIManager : MonoBehaviour//also make script for using buttons to click using mouse as well as keys
 {
-    public List<GameObject> buttonElements = new List<GameObject>();
+    public List<GameObject> buttonElements = new List<GameObject>();//use list to 
+    private int index;//used to index button elements to see which button is being selected
+
     public GameObject background;
+
+    //use positions to place pointer at their locations in canvas space
+    public Vector3 newGamePos;
+    public Vector3 loadGamePos;
+    public Vector3 optionsPos;
 
     void Start()
     {
@@ -14,7 +23,7 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //if input enter
     }
     public void newGame(){
 
@@ -27,5 +36,11 @@ public class UIManager : MonoBehaviour
     public void openOptions(){
 
     }
-    
+
+    public void incrementIndex(){
+
+    }
+    public void decrementIndex(){
+
+    }
 }
