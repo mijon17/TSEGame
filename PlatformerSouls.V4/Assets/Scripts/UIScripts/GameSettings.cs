41fd260e79777bd965eb6f29fde9a5d619d4db3f
ShootyBoi <79335581+ShootyBoi@users.noreply.github.com>
2022-02-27 13:06:50 +0000

    GameSettings initialisation
    creation of scriptable object

---
+++ PlatformerSouls.V4/Assets/Scripts/UIScripts/GameSettings.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(fileName = "GameSettings", menuName = "GameSettings", order = 1)]
+[Serializable]
+public class GameSettings : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //load preferences from file;
+    }
+
+
+}
--- PlatformerSouls.V4/Assets/Scripts/UIScripts/optionsManager.cs
+++ PlatformerSouls.V4/Assets/Scripts/UIScripts/optionsManager.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class optionsManager : MonoBehaviour
 {
+    private List<GameObject> mainMenuElements = new List<GameObject>();
+    public UIManager mainMenuManager;
+    public GameObject optionsPanel;
+    //private float volume;
+    //private int brightness
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainMenuElements = mainMenuManager.mainMenuElements;
+        //
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void back(){
+        optionsPanel.SetActive(false);
+        foreach(GameObject obj in mainMenuElements){
+            obj.SetActive(true);
+        }
+    }
+    public void apply(){
+        //save system preferences
+        //PlayerPrefs.SetFloat("volume", volume);
     }
 }
