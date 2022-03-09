5de48c3ac4ffa1e4e0c5de265220772903babd69
ShootyBoi <79335581+ShootyBoi@users.noreply.github.com>
2022-03-09 17:31:10 +0000

    tutorialLevel manager

---
+++ PlatformerSouls.V4/Assets/Scripts/tables/enemyManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyManager : MonoBehaviour
+{
+    private static int newId = 0;
+    public int ID;
+    public GameObject enemyType;
+    public Vector3 spawnPos;
+    public Vector3 spawnRot;
+    public bool alive;
+
+     
+    void Start()
+    {
+        ID = newId;
+        newId++;
+    }
+
+}
