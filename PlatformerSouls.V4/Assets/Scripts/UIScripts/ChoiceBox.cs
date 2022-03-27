dbeb58068001592204e208e8b27151d87e14f489
ShootyBoi <79335581+ShootyBoi@users.noreply.github.com>
2022-03-27 14:59:19 +0100

    choiceBox base functionality

--- PlatformerSouls.V4/Assets/Scripts/UIScripts/ChoiceBox.cs
+++ PlatformerSouls.V4/Assets/Scripts/UIScripts/ChoiceBox.cs
@@ -10,6 +10,9 @@ public class ChoiceBox : dialogueBoxScript
     public GameObject pointer;
     public int index = 0;
 
+    public Vector3 choice1Location;
+    public Vector3 choice2Location;
+
     void Start()
     {
         
@@ -22,12 +25,28 @@ public class ChoiceBox : dialogueBoxScript
         {
             if (index == 0)
             {
-
+                //choice 0 function
             }
             else if (index == 1)
             {
-
+                //choice 1 function
             }
+        }
+        if(Input.GetKeyDown(KeyCode.LeftArrow) && index == 0){
+            index = 1;
+            //change pointer location
+        }
+        else if(Input.GetKeyDown(KeyCode.LeftArrow) && index == 1){
+            index = 0;
+            //change pointer location
+        }
+        else if(Input.GetKeyDown(KeyCode.RightArrow) && index == 0){
+            index = 1;
+            //change pointer location
+        }
+        else if(Input.GetKeyDown(KeyCode.RightArrow) && index == 1){
+            index = 0;
+            //change pointer location
         }
     }
 }
