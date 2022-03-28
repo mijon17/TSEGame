5e0114410bf9a9c90334eb467c30e47657ccdd3a
ShootyBoi <79335581+ShootyBoi@users.noreply.github.com>
2022-03-28 20:21:15 +0100

    commented out dialogue box

--- PlatformerSouls.V4/Assets/Scripts/UIScripts/dialogueBoxScript.cs
+++ PlatformerSouls.V4/Assets/Scripts/UIScripts/dialogueBoxScript.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,3 +11,4 @@ public class dialogueBoxScript : MonoBehaviour
   
 }
 
+*/
